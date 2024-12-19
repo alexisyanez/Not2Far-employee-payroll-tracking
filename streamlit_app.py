5e4f31cc7d90fafeab8624467e3f65637c2be4c7
Alexis Yanez <adanilo.yanez@gmail.com>
2024-12-19 19:19:51 +0000

    Updating template to Payroll tracking

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ import pandas as pd
 import streamlit as st
 
 # Show app title and description.
-st.set_page_config(page_title="Support tickets", page_icon="🎫")
-st.title("🎫 Support tickets")
+st.set_page_config(page_title="Employee Payroll Tracking", page_icon="💵")
+st.title("💵 Employee Payroll Tracking")
 st.write(
     """
     This app shows how you can build an internal tool in Streamlit. Here, we are 
@@ -49,10 +49,10 @@ if "df" not in st.session_state:
 
     # Generate the dataframe with 100 rows/tickets.
     data = {
-        "ID": [f"TICKET-{i}" for i in range(1100, 1000, -1)],
-        "Issue": np.random.choice(issue_descriptions, size=100),
-        "Status": np.random.choice(["Open", "In Progress", "Closed"], size=100),
-        "Priority": np.random.choice(["High", "Medium", "Low"], size=100),
+        "ID": [f"Payroll-{i}" for i in range(1100, 1000, -1)],
+        "Employee": np.random.choice(issue_descriptions, size=100),
+        "Status": np.random.choice(["Unregistered", "In Progress", "Paid"], size=100),
+        "Department": np.random.choice(["Production", "Assembly", "Transportation", "Marketing", "Managment"], size=100),
         "Date Submitted": [
             datetime.date(2023, 6, 1) + datetime.timedelta(days=random.randint(0, 182))
             for _ in range(100)
@@ -66,13 +66,15 @@ if "df" not in st.session_state:
 
 
 # Show a section to add a new ticket.
-st.header("Add a ticket")
+st.header("Add a employee")
 
 # We're adding tickets via an `st.form` and some input widgets. If widgets are used
 # in a form, the app will only rerun once the submit button is pressed.
 with st.form("add_ticket_form"):
-    issue = st.text_area("Describe the issue")
-    priority = st.selectbox("Priority", ["High", "Medium", "Low"])
+    issue = st.text_area("First Name and Last Name format")
+    dpto = st.selectbox("Department", ["Production", "Assembly", "Transportation", "Marketing", "Managment"])
+    rates = list(range(1,101))
+    rate = st.selectbox(" Hour Rate US$", rates)
     submitted = st.form_submit_button("Submit")
 
 if submitted:
@@ -83,26 +85,27 @@ if submitted:
     df_new = pd.DataFrame(
         [
             {
-                "ID": f"TICKET-{recent_ticket_number+1}",
-                "Issue": issue,
-                "Status": "Open",
-                "Priority": priority,
+                "ID": f"Payrrol-{recent_ticket_number+1}",
+                "Employee": issue,
+                "Status": "Unpaid",
+                "Department": dpto,
+                "Hour Rate": rate,
                 "Date Submitted": today,
             }
         ]
     )
 
     # Show a little success message.
-    st.write("Ticket submitted! Here are the ticket details:")
+    st.write("Employee Submitted submitted! Here are the ticket details:")
     st.dataframe(df_new, use_container_width=True, hide_index=True)
     st.session_state.df = pd.concat([df_new, st.session_state.df], axis=0)
 
 # Show section to view and edit existing tickets in a table.
-st.header("Existing tickets")
-st.write(f"Number of tickets: `{len(st.session_state.df)}`")
+st.header("Existing payrolls")
+st.write(f"Number of payrolls: `{len(st.session_state.df)}`")
 
 st.info(
-    "You can edit the tickets by double clicking on a cell. Note how the plots below "
+    "You can edit the payrolls by double clicking on a cell. Note how the plots below "
     "update automatically! You can also sort the table by clicking on the column headers.",
     icon="✍️",
 )
@@ -116,14 +119,21 @@ edited_df = st.data_editor(
     column_config={
         "Status": st.column_config.SelectboxColumn(
             "Status",
-            help="Ticket status",
-            options=["Open", "In Progress", "Closed"],
+            help="Payroll status",
+            options=["Unregistered", "In Progress", "Paid"],
             required=True,
         ),
-        "Priority": st.column_config.SelectboxColumn(
-            "Priority",
-            help="Priority",
-            options=["High", "Medium", "Low"],
+        "Department": st.column_config.SelectboxColumn(
+            "Department",
+            help="Department",
+            options=["Production", "Assembly", "Transportation", "Marketing", "Managment"],
+            required=True,
+        ),
+        "Hour Rate": st.column_config.SelectboxColumn(
+            "Hour Rate",
+            help="Hour Rate",
+            rates = list(range(1,101))
+            options= rates, #["Production", "Assembly", "Transportation", "Marketing", "Managment"],
             required=True,
         ),
     },
@@ -136,14 +146,14 @@ st.header("Statistics")
 
 # Show metrics side by side using `st.columns` and `st.metric`.
 col1, col2, col3 = st.columns(3)
-num_open_tickets = len(st.session_state.df[st.session_state.df.Status == "Open"])
-col1.metric(label="Number of open tickets", value=num_open_tickets, delta=10)
+num_open_tickets = len(st.session_state.df[st.session_state.df.Status == "Uregistered"])
+col1.metric(label="Number of open payrolls", value=num_open_tickets, delta=10)
 col2.metric(label="First response time (hours)", value=5.2, delta=-1.5)
 col3.metric(label="Average resolution time (hours)", value=16, delta=2)
 
 # Show two Altair charts using `st.altair_chart`.
 st.write("")
-st.write("##### Ticket status per month")
+st.write("##### Payroll status per month")
 status_plot = (
     alt.Chart(edited_df)
     .mark_bar()
@@ -159,11 +169,11 @@ status_plot = (
 )
 st.altair_chart(status_plot, use_container_width=True, theme="streamlit")
 
-st.write("##### Current ticket priorities")
+st.write("##### Current payroll department")
 priority_plot = (
     alt.Chart(edited_df)
     .mark_arc()
-    .encode(theta="count():Q", color="Priority:N")
+    .encode(theta="count():Q", color="Department:N")
     .properties(height=300)
     .configure_legend(
         orient="bottom", titleFontSize=14, labelFontSize=14, titlePadding=5
